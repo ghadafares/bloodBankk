43004e5f64c921c660eca96dc88f9fb6a446c510
galil <galiliooo.ma@gmail.com>
2020-09-26 19:58:27 +0200

    relative to constrain

--- app/src/main/java/com/example/bloodbank/MainActivity.java
+++ app/src/main/java/com/example/bloodbank/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-    int timeout=4000;
+    int timeout=2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/bloodbank/login.java
+++ app/src/main/java/com/example/bloodbank/login.java
@@ -9,29 +9,30 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 public class login extends AppCompatActivity {
-  ImageView iv_newDonor;
-  ImageView iv_searchDonor;
+    ImageView iv_newDonor;
+    ImageView iv_searchDonor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-     iv_newDonor=findViewById(R.id.iv_newDonor);
-     iv_searchDonor=findViewById(R.id.iv_searchDonor);
+        iv_newDonor = findViewById(R.id.iv_newDonor);
+        iv_searchDonor = findViewById(R.id.iv_searchDonor);
 
-     iv_newDonor.setOnClickListener(new View.OnClickListener() {
-         @Override
-         public void onClick(View view) {
-             Intent intent=new Intent(login.this,sign_up.class);
-             startActivity(intent);
-         }
-     });
-     iv_searchDonor.setOnClickListener(new View.OnClickListener() {
-         @Override
-         public void onClick(View view) {
-             Intent intent=new Intent(login.this,search.class);
-             startActivity(intent);
-         }
-     });
+        iv_newDonor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(login.this, sign_up.class);
+                startActivity(intent);
+            }
+        });
+        iv_searchDonor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(login.this, search.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
